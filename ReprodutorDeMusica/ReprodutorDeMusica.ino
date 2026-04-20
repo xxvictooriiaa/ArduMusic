07cb9fdb613e4ff6b64d3868a42b6f411d19e423
Lauanda Nobre <113943598+lauandanobre@users.noreply.github.com>
2026-04-19 21:27:04 -0300

    Adicionando implementação inicial do display e mensagem hello world claro

--- ReprodutorDeMusica/ReprodutorDeMusica.ino
+++ ReprodutorDeMusica/ReprodutorDeMusica.ino
@@ -1,3 +1,4 @@
+
 /*
  * =========================================================================
  * PROJETO: Tocador de Música MP3 (Buzzer & Display)
@@ -11,16 +12,26 @@
  * VERSÃO: 0.1.0
  * =========================================================================
  */
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 #define BUZZER 9
+#define LED_VERDE 7
+#define LED_VERMELHA 8
 
 void setup()
 {
   Serial.begin(9600);
+  lcd.begin(16,2);//tamanho em linhas e colunas do lcd display
+  lcd.clear(); // limpar a tela
   pinMode(BUZZER, OUTPUT);
 }
 
 void loop()
 {
+  lcd.setCursor(3,0);
+  lcd.print("Hello world");
   // SOM
   tone(BUZZER, 800, 1000);
   delay(2000); 
